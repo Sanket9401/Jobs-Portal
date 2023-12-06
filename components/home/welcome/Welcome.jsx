85c86f1f3a7403d34801fd45a3e29b9aa9f5841f
Sanket Shinde <sanketshinde9401@gmail.com>
2023-12-06 22:08:45 +0530

    first commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./welcome.style";
+import { icons, images, sIZES } from "../../../constants";
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Sanket</Text>
+        <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
